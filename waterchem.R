dcd5ae34641ad3940899b3f48c8c0af339259855
TraciPopejoy <TraciPopejoy@ou.edu>
2018-11-15 18:08:16 -0600

    cleaning, checking models

--- waterchem.R
+++ waterchem.R
@@ -1,11 +1,9 @@
-library(readxl)
+library(readxl); library(tidyverse); library(ggplot2)
 treat<-read_excel("./data/CostMutData.xlsx",sheet="TankData")
 nitrogen<-read_xlsx("./data/WaterNutrients.xlsx", sheet="Ammonia")
 
-library(tidyverse)
 nitstand<-nitrogen %>% filter(Type=="standard")
 
-library(ggplot2)
 ggplot(nitstand, aes(x=ActualNH3.ug.L, y=x640nm))+
   geom_point()+geom_smooth(method="lm", formula=y~x)
 
@@ -64,19 +62,16 @@ WaterNutrients<-left_join(NH3WaterNuts,SRPWaterNuts) %>%
   left_join(treat[,c(1,7)])
 
 WNmelted<-WaterNutrients %>% gather(variable, value, -c(Tank,Week, NewTreat,Date)) %>%
-  mutate(Week.c=as.numeric(paste(Week)))%>%
-  select(Tank, Week, NewTreat, variable, value, Date)
+  mutate(Week.c=as.numeric(paste(Week)),
+         value.c=as.numeric(paste(value)))%>%
+  select(Tank, Week, NewTreat, variable, value.c, Date)
 WNmelted[WNmelted$variable=="Filt.element.ratio", "VarType"]="ratio"
 WNmelted[WNmelted$variable=="Unfilt.element.ratio", "VarType"]="ratio"
 
 ggplot(na.omit(WNmelted[WNmelted$VarType=="ratio" &
-                        WNmelted$Tank!="T",]), 
-       aes(x=Week, y=value, color=NewTreat)) + 
-  geom_boxplot() + facet_grid(~variable) +theme_bw()
-
-ggplot(WNmelted[is.na(WNmelted$VarType),], 
-       aes(x=Week, y=value, color=NewTreat)) + 
-  geom_boxplot() + facet_grid(~variable)+theme_bw()
+                        WNmelted$variable!="Unfilt.element.ratio",]), 
+       aes(x=Week, y=value.c, color=NewTreat)) + 
+  geom_boxplot() +theme_bw()
 
 fronteirstheme<-theme(axis.title.y=element_text(size=rel(.6)),
                       axis.title.x=element_text(size=rel(.6)),
@@ -90,63 +85,44 @@ nh3filt<-ggplot(WaterNutrients,
                 aes(x=Day, y=FilterdNH3ugL, color=NewTreat)) + 
   stat_summary(fun.y = mean, geom = "line")+
   scale_x_continuous(breaks = sort(unique(WaterNutrients$Day)), name="")+
-  scale_color_grey(start=0.4, end=.8, name="Treatment")+
-  scale_fill_grey(start=0.4, end=.8, 
+  scale_color_manual(values=col6, name="Treatment")+
+  scale_fill_manual(values=col6,
                   guide=guide_legend(override.aes=list(shape=c(23,22,21))), name="Treatment")+
-  stat_summary(aes(fill=NewTreat, shape=NewTreat), position=position_dodge(width=1))+ 
+  stat_summary(aes(fill=NewTreat, shape=NewTreat), position=position_dodge(width=1.75))+ 
   scale_shape_manual(name = "Group", values = c(23, 22, 21), guide=F)+
   geom_vline(xintercept = 0, linetype="dashed") +
-  ylab(expression("Filtered NH "[3]*" ug "%*%L^-1))+
+  ylab(expression("NH"[3]*"-N  ug "%*%L^-1))+
   fronteirstheme+theme(legend.background = element_rect(fill=NA),
                        legend.position = c(0, .746),
                        axis.title.x=element_text(size=rel(0)))
-nh3un<-ggplot(WaterNutrients,
-               aes(x=Day, y=UnFiltNH3ugL, color=NewTreat)) + 
-  stat_summary(fun.y = mean, geom = "line")+
-  scale_x_continuous(breaks = sort(unique(WaterNutrients$Day)), name="")+
-  scale_color_grey(start=0.4, end=.8, guide=F)+
-  scale_fill_grey(start=0.4, end=.8, guide=F)+
-  stat_summary(aes(fill=NewTreat, shape=NewTreat), position=position_dodge(width=1))+ 
-  scale_shape_manual(name = "Group", values = c(23, 22, 21), guide=F)+
-  geom_vline(xintercept = 0, linetype="dashed") +
-  ylab(expression("Total NH "[3]*" ug "%*%L^-1))+fronteirstheme+
-  theme(axis.title.x=element_text(size=rel(0)))
 srpfil<-ggplot(WaterNutrients,
                aes(x=Day, y=FilterdSRPugL, color=NewTreat)) + 
   stat_summary(fun.y = mean, geom = "line")+
-  scale_x_continuous(breaks = sort(unique(WaterNutrients$Day)),name="")+
-  scale_color_grey(start=0.4, end=.8, guide=F)+
-  scale_fill_grey(start=0.4, end=.8,guide=F)+
-  stat_summary(aes(fill=NewTreat, shape=NewTreat), position=position_dodge(width=1))+ 
-  scale_shape_manual(name = "Group", values = c(23, 22, 21), guide=F)+
-  geom_vline(xintercept = 0, linetype="dashed") +
-  ylab(expression("Filtered SRP ug "%*%L^-1))+fronteirstheme+
-  theme(axis.title.x=element_text(size=rel(0)))
-srpun<-ggplot(WaterNutrients,
-                aes(x=Day, y=UnFiltSRPugL, color=NewTreat)) + 
-  stat_summary(fun.y = mean, geom = "line")+
   scale_x_continuous(breaks = sort(unique(WaterNutrients$Day)), name="Sampling Day")+
-  scale_color_grey(start=0.4, end=.8, guide=F)+
-  scale_fill_grey(start=0.4, end=.8, guide=F)+
-  stat_summary(aes(fill=NewTreat, shape=NewTreat), position=position_dodge(width=1))+ 
+  scale_y_continuous(breaks=c(50,100,200,300,400,500,575))+
+  scale_color_manual(values=col6, guide=F)+
+  scale_fill_manual(values=col6,guide=F)+
+  stat_summary(aes(fill=NewTreat, shape=NewTreat), position=position_dodge(width=1.75))+ 
   scale_shape_manual(name = "Group", values = c(23, 22, 21), guide=F)+
   geom_vline(xintercept = 0, linetype="dashed") +
-  ylab(expression("Total SRP ug "%*%L^-1))+fronteirstheme
-wnplot<-plot_grid(nh3filt, nh3un, srpfil,srpun, ncol=1,labels = NULL)
-ggsave("Fig1.tiff", wnplot, width=3.34, height=7, dpi=300)
+  ylab(expression("SRP ug "%*%L^-1))+fronteirstheme
+wnplot<-plot_grid(nh3filt, srpfil, ncol=1,labels = NULL)
+ggsave("Fig1.tiff", wnplot, width=3.34, height=5, dpi=300)
 
-#### LINEAR MODELS ####
+### LINEAR MODELS
 ## data assumptions
+hist(WaterNutrients$FilterdSRPugL)
+shapiro.test(log10(WaterNutrients$FilterdSRPugL))
+hist(WaterNutrients$FilterdNH3ugL)
+hist(log10(WaterNutrients$FilterdNH3ugL))
+shapiro.test(log10(WaterNutrients$FilterdNH3ugL))
 
 ## models
 library(lme4); library(emmeans); library(lmerTest) 
 WNmodFsrp<-lmer(FilterdSRPugL~NewTreat + Day+ (1|Tank), data=WaterNutrients)
 anova(WNmodFsrp)
 
-WNmodUsrp<-lmer(UnFiltSRPugL~NewTreat + Day +(1|Tank), data=WaterNutrients)
-anova(WNmodUsrp)
-
-WNmodFnh3<-lmer(FilterdNH3ugL~NewTreat + Day + (1|Tank), data=WaterNutrients)
+WNmodFnh3<-lmer(log10(FilterdNH3ugL)~NewTreat + Day + (1|Tank), data=WaterNutrients)
 anova(WNmodFnh3)
 summary(WNmodFnh3)
 ranova(WNmodFnh3)
@@ -154,40 +130,8 @@ ranova(WNmodFnh3)
 WNfNh<-emmeans(WNmodFnh3, pairwise~NewTreat, adjust="tukey")
 CLD(WNfNh, alpha=.05, Letters=letters, adjust="tukey")
 
-WNmodUnh3<-lmer(UnFiltNH3ugL~NewTreat+ Day+ (1|Tank), data=WaterNutrients)
-anova(WNmodUnh3)
-
 ##### assumptions
-hist(residuals(MetMod),col="darkgrey") #skewed
-plot(fitted(MetMod), residuals(MetMod)) 
-
-
-WaterNutrients %>% filter(Week==3 | Week==2) %>% group_by(Tank,Week) %>%
-  select(Tank,Week,FilterdNH3ugL) %>% mutate(id=1:n()) %>%
-  spread(Week, FilterdNH3ugL) %>% 
-  mutate(change=(`3`-`2`)/`3`*100) %>% left_join(treat, by="Tank") %>%
-  select(Tank, NewTreat, `3`,`2`,change) %>% 
-  filter(!is.na(change), Tank!="W") %>% group_by(NewTreat) %>%
-  summarize(meanChange=mean(change),
-            max=max(change),
-            min=min(change))
-
-library(vegan)
-WCcom<-WNmelted %>% mutate(T.W=paste(Tank, Week, sep="."),
-                    nob=seq(1:nrow(.)))%>% 
-  group_by(T.W, NewTreat) %>%
-  filter(is.na(VarType))%>%
-  select(-Date, -VarType) %>% spread(variable, value)%>%
-  summarize_if(is.numeric, mean, na.rm=T) %>% select(-nob) %>%
-  mutate(Tank=substring(T.W, 1,1), Week=substring(T.W, 3,3))
-WCcom1<-WCcom[!is.na(WCcom$UnFiltSRPugL),]
-WCrda<-rda(WCcom1[,3:6]~WCcom1$NewTreat, data=WCcom1)
-
-WCnmds<-metaMDS(!is.na(WCcom[,3:6]),k=2)
-ordiplot(WCnmds,type="n")
-orditorp(WCnmds,display="species",col="red",air=0.01)
-orditorp(WCnmds,display="sites",cex=1.25,air=0.01)  
-ordihull(WCnmds,groups=WCcom$NewTreat,draw="polygon",col="grey90",label=T)
-orditorp(WCnmds,display="sites",col=c(rep(rainbow(8), 18)),air=0.01,cex=1.25)
-
-         
+qqnorm(resid(WNmodFsrp)); qqline(resid(WNmodFsrp))
+hist(residuals(WNmodFnh3),col="darkgrey") #skewed
+plot(fitted(WNmodFnh3), residuals(WNmodFnh3)) 
+qqnorm(resid(WNmodFnh3)); qqline(resid(WNmodFnh3))
