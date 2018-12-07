a72e3e5936160be173ed02d7475c196a47cf2320
TraciPopejoy <TraciPopejoy@ou.edu>
2018-12-07 12:21:12 -0600

    commented, checked assumptions

--- waterchem.R
+++ waterchem.R
@@ -16,21 +16,18 @@ NH3raw<-nitrogen %>% mutate(PredNH3=((x640nm-Nb)/Na)) %>% filter(Type=="sample")
   mutate(Tank=substring(Water.Sample,1,1),
          WaterType=substring(Water.Sample,3,6))
 
-NH3Filtered<-NH3raw %>% filter(WaterType=="WCNF") %>% 
+NH3WaterNuts<-NH3raw %>% 
   select(-Type, -Location, -entry.position, -ActualNH3.ug.L) %>%
-  mutate(Week=substring(Water.Sample, 10))
-NH3UnFilt<-NH3raw %>% filter(WaterType=="WCN ") %>% 
-  select(-Type, -Location, -entry.position, -ActualNH3.ug.L) %>%
-  mutate(Week=substring(Water.Sample, 9,9))
-NH3WaterNuts<- left_join(NH3Filtered, NH3UnFilt, by=c("Tank","Week")) %>%
-  select(Tank,Week, PredNH3.x, PredNH3.y)
-names(NH3WaterNuts)<-c("Tank","Week","FilterdNH3ugL","UnFiltNH3ugL")
-  
+  mutate(Week=case_when(WaterType=="WCNF"~substring(Water.Sample, 10),
+                        WaterType=="WCN "~substring(Water.Sample, 9,9))) %>%
+  spread(WaterType, PredNH3) %>% select(-x640nm) %>% group_by(Tank, Week) %>%
+  summarize(FilterdNH3ugL=mean(`WCNF`, na.rm=T), 
+            UnFiltNH3ugL=mean(`WCN `, na.rm=T))
+
+####phosphorus
 phosphorus<-read_xlsx("./data/WaterNutrients.xlsx", sheet="SRP")
-library(tidyverse)
 phosstand<-phosphorus %>% filter(Type=="standard")%>% filter(ActualSRP.ug.L!=640)
 
-library(ggplot2)
 ggplot(phosstand, aes(x=ActualSRP.ug.L, y=x885nm))+
   geom_point()+geom_smooth(method="lm")
 
@@ -43,35 +40,24 @@ SRPraw<-phosphorus %>% mutate(PredSRPugL=((x885nm-Pb)/Pa)) %>% filter(Type=="sam
   mutate(Tank=substring(Water.Sample,1,1),
          WaterType=substring(Water.Sample,3,6))
 
-SRPFiltered<-SRPraw %>% filter(WaterType=="WCNF") %>% 
+SRPFiltered<-SRPraw %>%
   select(-Type, -Location, -entry.position, -ActualSRP.ug.L) %>%
-  mutate(Week=substring(Water.Sample, 10))
-SRPUnFilt<-SRPraw %>% filter(WaterType=="WCN ") %>% 
-  select(-Type, -Location, -entry.position, -ActualSRP.ug.L) %>%
-  mutate(Week=substring(Water.Sample, 9,9))
-SRPWaterNuts<- left_join(SRPFiltered, SRPUnFilt, by=c("Tank","Week")) %>%
-  select(Tank,Week, PredSRPugL.x, PredSRPugL.y)
-names(SRPWaterNuts)<-c("Tank","Week","FilterdSRPugL","UnFiltSRPugL")
-
+  mutate(Week=case_when(WaterType=="WCNF"~substring(Water.Sample, 10),
+                        WaterType=="WCN "~substring(Water.Sample, 9,9))) %>%
+  spread(WaterType, PredSRPugL) %>% select(-x885nm) %>% group_by(Tank, Week) %>%
+  summarize(FilterdNH3ugL=mean(`WCNF`, na.rm=T), 
+            UnFiltNH3ugL=mean(`WCN `, na.rm=T))
+head(physchem) #in physiochem.R script
+#physchem<-read_excel("./data/CostMutData.xlsx",sheet = "PhysioChem")
+#physchem[,"Date"]<-as.Date(physchem$Time)
+library(lubridate)
 WaterNutrients<-left_join(NH3WaterNuts,SRPWaterNuts) %>% 
   mutate(Filt.element.ratio=FilterdNH3ugL/FilterdSRPugL*(30.97/14.01),
          Unfilt.element.ratio=UnFiltNH3ugL/UnFiltSRPugL*(30.97/14.01),
          Week.c=as.numeric(paste(Week))) %>%
   left_join(physchem, by=c("Week.c"="Week", "Tank"))%>%
   mutate(Day=as.numeric(Date-ymd("2018-07-02"))) %>%
-  left_join(treat[,c(1,7)])
-
-WNmelted<-WaterNutrients %>% gather(variable, value, -c(Tank,Week, NewTreat,Date)) %>%
-  mutate(Week.c=as.numeric(paste(Week)),
-         value.c=as.numeric(paste(value)))%>%
-  select(Tank, Week, NewTreat, variable, value.c, Date)
-WNmelted[WNmelted$variable=="Filt.element.ratio", "VarType"]="ratio"
-WNmelted[WNmelted$variable=="Unfilt.element.ratio", "VarType"]="ratio"
-
-ggplot(na.omit(WNmelted[WNmelted$VarType=="ratio" &
-                        WNmelted$variable!="Unfilt.element.ratio",]), 
-       aes(x=Week, y=value.c, color=NewTreat)) + 
-  geom_boxplot() +theme_bw()
+  left_join(treat[,c(1,7)]) %>% filter(Week!=4)
 
 fronteirstheme<-theme(axis.title.y=element_text(size=rel(.6)),
                       axis.title.x=element_text(size=rel(.6)),
@@ -80,7 +66,7 @@ fronteirstheme<-theme(axis.title.y=element_text(size=rel(.6)),
                       legend.direction = "vertical", legend.position =c(0,.74),
                       legend.text = element_text(size=rel(.5)),
                       legend.title= element_text(size=rel(.5)))
-
+#col6<-c("darkgrey","#5389a6","forestgreen") #col6 in Producers.R
 nh3filt<-ggplot(WaterNutrients,
                 aes(x=Day, y=FilterdNH3ugL, color=NewTreat)) + 
   stat_summary(fun.y = mean, geom = "line")+
@@ -106,32 +92,34 @@ srpfil<-ggplot(WaterNutrients,
   scale_shape_manual(name = "Group", values = c(23, 22, 21), guide=F)+
   geom_vline(xintercept = 0, linetype="dashed") +
   ylab(expression("SRP ug "%*%L^-1))+fronteirstheme
+library(cowplot)
 wnplot<-plot_grid(nh3filt, srpfil, ncol=1,labels = NULL)
-ggsave("Fig1.tiff", wnplot, width=3.34, height=5, dpi=300)
+ggsave("DeathFigures/Fig2.tiff", wnplot, width=3.34, height=5, dpi=300)
 
-### LINEAR MODELS
-## data assumptions
-hist(WaterNutrients$FilterdSRPugL)
-shapiro.test(log10(WaterNutrients$FilterdSRPugL))
-hist(WaterNutrients$FilterdNH3ugL)
-hist(log10(WaterNutrients$FilterdNH3ugL))
-shapiro.test(log10(WaterNutrients$FilterdNH3ugL))
-
+##### LINEAR MODELS #####
 ## models
 library(lme4); library(emmeans); library(lmerTest) 
-WNmodFsrp<-lmer(FilterdSRPugL~NewTreat + Day+ (1|Tank), data=WaterNutrients)
-anova(WNmodFsrp)
+#nitrogen
+hist(log10(WaterNutrients$FilterdNH3ugL))
 
 WNmodFnh3<-lmer(log10(FilterdNH3ugL)~NewTreat + Day + (1|Tank), data=WaterNutrients)
 anova(WNmodFnh3)
 summary(WNmodFnh3)
-ranova(WNmodFnh3)
 
 WNfNh<-emmeans(WNmodFnh3, pairwise~NewTreat, adjust="tukey")
 CLD(WNfNh, alpha=.05, Letters=letters, adjust="tukey")
 
+#phosphorus
+WNmodFsrp<-lmer(FilterdSRPugL~NewTreat + Day + (1|Tank), data=WaterNutrients)
+anova(WNmodFsrp)
+summary(WNmodFsrp)
+
 ##### assumptions
+#nitrogen
+hist(residuals(WNmodFnh3),col="darkgrey") #normal distribution?
+plot(fitted(WNmodFnh3), residuals(WNmodFnh3)) #heteroscadastic
+qqnorm(resid(WNmodFnh3)); qqline(resid(WNmodFnh3)) #large values poorly predicted
+#phosphorus
+hist(residuals(WNmodFnh3),col="darkgrey") #normal distribution?
+plot(fitted(WNmodFsrp), residuals(WNmodFsrp)) #heteroscadastic
 qqnorm(resid(WNmodFsrp)); qqline(resid(WNmodFsrp))
-hist(residuals(WNmodFnh3),col="darkgrey") #skewed
-plot(fitted(WNmodFnh3), residuals(WNmodFnh3)) 
-qqnorm(resid(WNmodFnh3)); qqline(resid(WNmodFnh3))
