5ed98e8a47dac1e2b8bc8d977b6361306cc54c3a
Traci P <TraciPopejoy@ou.edu>
2019-03-18 13:26:17 -0500

    made 'the best map yet' according to the hubby

--- MapMassMort.R
+++ MapMassMort.R
@@ -82,7 +82,7 @@ plot(OKflowLines[OKflowLines@data$gnis_name=="Red River",], add=T, col='lightgre
 
 library(ggmap)
 #these calls 'tidy' the data so I can plot it with ggplot
-usa <- map_data("usa")
+usa <- map_data("usa", projection="+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0")
 states <- map_data("state")
 gatidy<-broom::tidy(GArivers) #lots of warnings about character strings
 GAflow<-broom::tidy(GAflowLines) #lots of warnings about character strings
@@ -97,57 +97,100 @@ mexico<-map_data("world",'mexico')
 usstreams<-broom::tidy(USflow8) #lots of warnings about character strings
 
 #make the continental map to show the distance between our two study sites
+xlimokga <- c(-95.82276, -94.18, -85,-83.65) 
+ylimokga <- c(33.75, 34.75, 30.6996, 32.3)
+devtools::install_github("3wen/legendMap")
+library(legendMap)
 bigmap<-ggplot() + 
   geom_polygon(data=states, aes(x=long, y=lat, group=group),
-               fill="white", color="darkgrey")+
+               fill="white", color="darkgrey", size=0.3)+
   geom_polygon(data=canada, aes(x=long, y=lat, group=group),
-               fill="darkgreen", color="darkgrey")+
+               fill="lightgrey", color="darkgrey")+
   geom_polygon(data=mexico, aes(x=long, y=lat, group=group), 
-               fill="darkgreen", color="darkgrey")+
+               fill="lightgrey", color="darkgrey")+
   geom_polygon(data = usa, aes(x=long, y = lat, group = group), 
-               fill=NA, color="black") +
-  geom_polygon(data = okbasins, aes(x=long, y = lat, group = group), 
-               fill="black", color="black") +
-  geom_polygon(data = gabasins, aes(x=long, y = lat, group = group),
-               fill="black", color="black") +
+               fill=NA, color="black", size=0.25) +
+  #geom_polygon(data = okbasins, aes(x=long, y = lat, group = group), 
+  #             fill="black", color="black") +
+  #geom_polygon(data = gabasins, aes(x=long, y = lat, group = group),
+  #             fill="black", color="black") +
   geom_path(data = usstreams, aes(x=long, y = lat, group = group), 
-            color="lightblue", size=1.3) +
-  geom_text(aes(x=c(-97,-82.5), y=c(34.75,32.55)), 
-            label=c("A","B"))+
+            color="darkcyan", size=0.4) +
+  geom_text(aes(x=c(-96.85,-82.53), y=c(34.77,32.55)),
+            label=c("A","B"), size=2)+
+  geom_rect(data=data.frame(),
+            aes(xmin=xlimokga[1], xmax=xlimokga[2], 
+                ymin=ylimokga[1], ymax=ylimokga[2]),
+                fill = NA, color="red")+
+  geom_rect(data=data.frame(),
+            aes(xmin=xlimokga[3], xmax=xlimokga[4], 
+                ymin=ylimokga[3], ymax=ylimokga[4]),
+                fill = NA, color="red")+
   xlab("Longitude")+ylab("Latitude")+
-  coord_map(xlim= c(-125.6813, -66.007), ylim=c(24.1299,50.3832))+theme_bw()
+  coord_map(xlim= c(-125.6813, -66.007), ylim=c(24.1299,50.3832))+
+  theme(panel.border = element_rect(colour = "black", fill=NA),
+        panel.background = element_rect(fill = NA, colour = NA),
+        axis.text.x = element_blank(),
+        axis.text.y = element_blank(), axis.ticks.x = element_blank(),
+        axis.ticks.y = element_blank(), axis.title = element_blank())
+ggsave("bigmap.tiff", bigmap,dpi=300, width=2.5,height=2)
 okmap<-ggplot()+
-  geom_path(data=OKflow, aes(x=long, y=lat, group=group), size=1.3, 
+  geom_path(data=OKflow, aes(x=long, y=lat, group=group), 
             color="lightblue")+
-  geom_path(data = oktidy, aes(x = long, y = lat, group=group), size=1.3)+
+  geom_path(data = oktidy, aes(x = long, y = lat, group=group))+
   geom_polygon(data=oklakestidy, aes(x=long,y=lat,group=group), 
                fill="lightblue")+
   geom_polygon(data=states, aes(x=long, y=lat, group=group), fill=NA, color="darkgrey")+
   geom_point(aes(x=c(-95.354508,-95.062153), y=c(34.574689,34.6569991)), 
-             color="black", fill="white",size=3, shape=24)+
+             color="black", fill="white",size=1.5, shape=24)+
   geom_text(aes(x=c(-95.33,-95.01), y=c(34.522,34.611)), 
-          label=c("K3","K2"))+
+          label=c("K3","K2"), size=3)+
   xlab("Longitude")+ylab("Latitude")+
-  coord_map(xlim=c(-95.7, -94.3),ylim=c(33.8,34.75))+theme_bw()
+  coord_map(xlim=c(-95.7, -94.22),ylim=c(33.87,34.75))+theme_bw()+
+  scale_bar(lon = -95.65, lat = 33.95, distance_lon = 10, distance_lat = 3, 
+          distance_legend = 6, dist_unit = "km", legend_size=2,
+          arrow_length = 10, arrow_distance=9, arrow_north_size = 4)+
+  theme(panel.border = element_rect(colour = "black", fill=NA),
+        panel.background = element_rect(fill = NA, colour = NA),
+        panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
+        axis.text.x = element_blank(),
+        axis.text.y = element_blank(), axis.ticks.x = element_blank(),
+        axis.ticks.y = element_blank(), axis.title = element_blank(),
+        rect = element_blank(),
+        plot.margin = unit(0 * c(-1.5, -1.5, -1.5, -1.5), "lines"))
+ggsave("okmap2.tiff", okmap,dpi=300, width=3, height=3.3)
 gamap<-ggplot()+
-  geom_path(data=GAflow, aes(x=long, y=lat, group=group), size=1.3, 
+  geom_path(data=GAflow, aes(x=long, y=lat, group=group), 
             color="lightblue")+
-  geom_path(data = gatidy, aes(x = long, y = lat, group=group), size=1.25)+
-  geom_polygon(data=galakestidy, aes(x=long,y=lat,group=group), fill="lightblue", 
-               size=1.2)+
+  geom_path(data = gatidy, aes(x = long, y = lat, group=group))+
+  geom_polygon(data=galakestidy, aes(x=long,y=lat,group=group), fill="lightblue")+
   geom_polygon(data=states, aes(x=long, y=lat, group=group), fill=NA, color="darkgrey")+
   xlab("Longitude")+ylab("Latitude")+
-  coord_map(xlim=c(-85, -83.65),ylim=c(30.70,32.3))+theme_bw()
-BigMapF<-grid.arrange(bigmap, top="Continental USA")
-sites<-plot_grid(okmap,gamap, labels=c("A","B"), nrow=1)
-mapmatrix<-plot_grid(BigMapF,sites, ncol=1)
-ggsave("test.tiff",mapmatrix)
+  coord_map(xlim=c(-85.2, -83.65),ylim=c(30.70,32.3))+theme_bw()+
+  scale_bar(lon = -84.5, lat = 30.8,
+            distance_lon =15, distance_lat = 3,
+            distance_legend = 7, dist_unit = "km", legend_size=2,
+            arrow_length = 10,arrow_distance=13.5, arrow_north_size = 4)+
+  theme(panel.border = element_rect(colour = "black", fill=NA),
+        panel.background = element_rect(fill = NA, colour = NA), 
+        panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
+        axis.text.x = element_blank(),
+        axis.text.y = element_blank(), axis.ticks.x = element_blank(),
+        axis.ticks.y = element_blank(), axis.title = element_blank(),
+        rect = element_blank(),
+        plot.margin = unit(0 * c(-1.5, -1.5, -1.5, -1.5), "lines"))
+gamap
 
-#add labels
-#add cities?
+
+leftmap<-plot_grid(okmap, bigmap, rel_heights =c(.75,.25), ncol=1)
+totalMap<-plot_grid(leftmap,gamap, nrow=1, labels="AUTO")
+ggsave("testingfuck.tiff", totalMap, height=3.5)
+streams<-plot_grid(okmap,gamap, nrow=1, labels="AUTO")
+ggsave("streams.tiff",streams,height=3.5)
+
 library(gridExtra)
-p1<-arrangeGrob(, okmap, gamap, layout_matrix = rbind(c(1,1),c(2,3)),
-                respect=TRUE)
+p1<-arrangeGrob(bigmap, okmap, gamap, layout_matrix = rbind(c(2,3),c(1,3)),
+                respect=TRUE, heights=c())
 ggsave("testmap.tiff",p1)
 
 library(LifeTables)
